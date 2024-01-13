d75ebc46e72508f4d4d000301f985c85bd972f0b
Geetanj <geetanj16sharma@gmail.com>
2024-01-13 12:26:35 +0530

    Feature added: Daily Employee Wage

--- employee_wage.sh
+++ employee_wage.sh
@@ -3,8 +3,16 @@ isPresent=1
 checkAttendance=$((RANDOM%2))
 if [ $isPresent -eq $checkAttendance ] 
 
-then echo "Employee is present"
+then echo -e "Employee is present\n"
 
-else echo "Employee is absent"
+else echo -e "Employee is absent\n"
 
 fi
+
+
+echo "Calculate Daily Wage"
+wage_perhour=20
+full_dayhour=8
+
+mul=$(($wage_perhour*$full_dayhour))
+echo -e "Daily Employee Wage $mul\n"
