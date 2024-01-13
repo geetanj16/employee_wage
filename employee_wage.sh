95a5ecf695e8b8da618183bafe64fc434839c134
Geetanj <geetanj16sharma@gmail.com>
2024-01-13 12:48:41 +0530

    Feature added: Switch Case

--- employee_wage.sh
+++ employee_wage.sh
@@ -9,15 +9,25 @@ else echo -e "Employee is absent\n"
 
 fi
 
+echo "Full-Time Employee: Enter 1"
+echo -e "Part-Time Employee: Enter 2\n"
 
-echo "Calculate Daily Wage"
-wage_perhour=20
+read number
+case $number in 
+
+1)
+
+
+echo "Calculate Daily Wage" 
+wage_perhour=20 
 full_dayhour=8
 
 mul=$(($wage_perhour*$full_dayhour))
 echo -e "Daily Employee Wage $mul\n"
 
+;;
 
+2)
 echo "Calculate Part-Time Employee Wage"
 echo "Enter your part-time working hour"
 read part_time
@@ -27,4 +37,5 @@ wage_perhour=20
 mul=$(($part_time*$wage_perhour))
 echo -e "Part-Time Employee Wage $mul\n"
 
+esac
 
