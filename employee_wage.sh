18682d94d3515f8bc38fcf1bf6394268ada8e523
Geetanj <geetanj16sharma@gmail.com>
2024-01-13 11:53:20 +0530

    Feature added: attendance

---
+++ employee_wage.sh
@@ -0,0 +1,10 @@
+echo "Welcome to Employee Wage Computation"
+isPresent=1
+checkAttendance=$((RANDOM%2))
+if [ $isPresent -eq $checkAttendance ] 
+
+then echo "Employee is present"
+
+else echo "Employee is absent"
+
+fi
